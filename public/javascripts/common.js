9ece13b937d57c96f8131955cd456b9943648043
Jokcy <jokcylou@hotmail.com>
2017-05-06 19:57:11 +0800

    clear sample code

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -57,40 +57,4 @@
 
   global._ = global.util = util
 
-
-  var navLoginItem = document.querySelector('#nav-login-item > a')
-  var loginDropDown = document.getElementById('user-login-dropdown')
-  var loginButton = document.getElementById('login-button')
-  var loginInput = document.getElementById('input-token')
-  var loginUserInfo = document.querySelector('.login-user-info')
-  var loginUserAvatar = document.querySelector('.login-user-avatar > img')
-  var loginUserName = document.querySelector('.login-user-name')
-  var notLogin = document.querySelector('.not-login')
-
-  // 绑定登陆按钮，控制下拉框的弹出或者因此
-  navLoginItem.addEventListener('click', function (e) {
-    e.preventDefault()
-    var display = loginDropDown.style.display
-    if (e.target)
-    loginDropDown.style.display = display === 'block' ? 'none' : 'block'
-  })
-
-  // 这一步我们读取输入的accesstoken，然后进行登陆
-  loginButton.addEventListener('click', function (e) {
-    e.preventDefault()
-    var token = loginInput.value
-    util.ajax({
-      type: 'POST',
-      url: '/api/login',
-      data: {
-        accesstoken: token
-      },
-      success: function (user) {
-        if (user.success) {
-          window.location.reload()
-        }
-      }
-    })
-  })
-
 })(window)
